09dd5b61c42948fd679ddc5549b5de4e566ed02a
taehee1 <163071930+taehee1@users.noreply.github.com>
2024-03-19 12:42:22 +0900

    완성

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public static int score = 0;
+    public static int bestscore = 0;
     Text uiText;
 
     void Start()
     {
         uiText = GetComponent<Text>();
+        score = 0;
     }
 
     void Update()
--- Assets/scripts/BirdJump.cs
+++ Assets/scripts/BirdJump.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BirdJump : MonoBehaviour
 {
     Rigidbody2D rb;
+    AudioSource audioSource;
 
     public float JumpPower;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -17,6 +21,16 @@ public class BirdJump : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * JumpPower;
+            audioSource.Play();
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Score.score > Score.bestscore)
+        {
+            Score.bestscore = Score.score;
+        }
+        SceneManager.LoadScene("GameOverScene");
     }
 }
